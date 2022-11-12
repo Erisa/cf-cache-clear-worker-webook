0efea9ea8d1735e8729c1caf81c7a60814c1ae84
Erisa A <erisa@erisa.uk>
2022-11-12 17:26:36 +0000

    Migrate to a module worker

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
 
-async function handleRequest(request) {
-  var url = new URL(request.url)
-  if (url.pathname == '/' + SECRET) {
-    var headers = new Headers({
-      Authorization: 'Bearer ' + CF_API_KEY,
-      'Content-Type': 'application/json',
-    })
+export default {
+  async fetch(request, env, ctx) {
+    var url = new URL(request.url)
+    if (url.pathname == '/' + env.SECRET) {
+      var headers = new Headers({
+        Authorization: 'Bearer ' + env.CF_API_KEY,
+        'Content-Type': 'application/json',
+      })
 
-    var cfReq = fetch(
-      'https://api.cloudflare.com/client/v4/zones/' + CF_ZONE + '/purge_cache',
-      {
-        headers: headers,
-        method: 'DELETE',
-        body: '{"purge_everything": true}',
-      },
-    )
-
-    return cfReq
-  } else {
-    return new Response('Unauthorized', {
-      status: 401,
-      headers: { 'content-type': 'text/plain' },
-    })
+      return fetch(
+        'https://api.cloudflare.com/client/v4/zones/' + env.CF_ZONE + '/purge_cache',
+        {
+          headers: headers,
+          method: 'DELETE',
+          body: '{"purge_everything": true}',
+        },
+      )
+    } else {
+      return new Response('Unauthorized', {
+        status: 401,
+        headers: { 'content-type': 'text/plain' },
+      })
+    }
   }
 }
