cf985dd7435ad7ce68d85219b64dea1900e2f308
Erisa A <erisa@erisa.uk>
2022-11-12 18:22:51 +0000

    Convert indentation to tabs

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 
 export default {
-  async fetch(request, env, ctx) {
-    var url = new URL(request.url)
-    if (url.pathname == '/' + env.SECRET) {
-      var headers = new Headers({
-        Authorization: 'Bearer ' + env.CF_API_KEY,
-        'Content-Type': 'application/json',
-      })
+	async fetch(request, env, ctx) {
+		var url = new URL(request.url)
+		if (url.pathname == '/' + env.SECRET) {
+			var headers = new Headers({
+				Authorization: 'Bearer ' + env.CF_API_KEY,
+				'Content-Type': 'application/json',
+			})
 
-      return fetch(
-        'https://api.cloudflare.com/client/v4/zones/' + env.CF_ZONE + '/purge_cache',
-        {
-          headers: headers,
-          method: 'DELETE',
-          body: '{"purge_everything": true}',
-        },
-      )
-    } else {
-      return new Response('Unauthorized', {
-        status: 401,
-        headers: { 'content-type': 'text/plain' },
-      })
-    }
-  }
+			return fetch(
+				'https://api.cloudflare.com/client/v4/zones/' + env.CF_ZONE + '/purge_cache',
+				{
+					headers: headers,
+					method: 'DELETE',
+					body: '{"purge_everything": true}',
+				},
+			)
+		} else {
+			return new Response('Unauthorized', {
+				status: 401,
+				headers: { 'content-type': 'text/plain' },
+			})
+		}
+	}
 }
